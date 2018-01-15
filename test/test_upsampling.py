60170783aa5e272b8199d689e39d0b6cab10ce9d
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2018-01-15 12:29:34 +0900

    update test

--- test/test_upsampling.py
+++ test/test_upsampling.py
@@ -1,18 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 Tomoki Hayashi (Nagoya University)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
 import numpy as np
+import pytest
 import torch
-import matplotlib.pyplot as plt
 from torch.autograd import Variable
 
 from wavenet import UpSampling, initialize
 
-aux = np.random.randn(1, 28, 1000)
-conv = UpSampling(80)
-conv.apply(initialize)
-batch = Variable(torch.from_numpy(aux).float())
-out = conv(batch)
-out = out.data.numpy()
-plt.subplot(1, 2, 1)
-plt.imshow(aux.reshape(28, -1)[3:], aspect="auto")
-plt.subplot(1, 2, 2)
-plt.imshow(out.reshape(28, -1)[3:], aspect="auto")
-plt.show()
+
+def test_upsampling():
+    aux = np.random.randn(1, 28, 1000)
+    conv = UpSampling(10)
+    conv.apply(initialize)
+    batch = Variable(torch.from_numpy(aux).float())
+    out = conv(batch)
+    out = out.data.numpy()
+    assert out.shape[-1] == aux.shape[-1] * 10
