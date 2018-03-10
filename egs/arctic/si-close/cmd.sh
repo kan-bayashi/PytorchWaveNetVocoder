dda198d9b834aedf13e50372f3d6ae4c6178322d
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2018-03-10 22:36:31 +0900

    restored removed line

--- egs/arctic/si-close/cmd.sh
+++ egs/arctic/si-close/cmd.sh
@@ -13,6 +13,7 @@
 # for local
 export train_cmd="run.pl"
 export cuda_cmd="run.pl --gpu 1"
+export max_jobs=1
 
 # for slurm (you can change configuration file "conf/slurm.conf")
 # export train_cmd="slurm.pl --config conf/slurm.conf"
