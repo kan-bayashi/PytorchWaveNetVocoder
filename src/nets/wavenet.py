fixed a redundant calculation in generation

src/nets/wavenet.py
@@ -344,7 +344,7 @@ class WaveNet(nn.Module):
         samples = x[0]
         start = time.time()
         for i in range(n_samples):
-            output = samples[-self.kernel_size * 2 - 1:].unsqueeze(0)
+            output = samples[-self.kernel_size * 2 + 1:].unsqueeze(0)
             output = self._preprocess(output)
             h_ = h[:, :, samples.size(0) - 1].contiguous().view(1, self.n_aux, 1)
             output_buffer_next = []
@@ -436,7 +436,7 @@ class WaveNet(nn.Module):
         end_samples = []
         start = time.time()
         for i in range(max_n_samples):
-            output = samples[:, -self.kernel_size * 2 - 1:]
+            output = samples[:, -self.kernel_size * 2 + 1:]
             output = self._preprocess(output)  # B x C x T
             h_ = h[:, :, samples.size(-1) - 1].contiguous().unsqueeze(-1)  # B x C x 1
             output_buffer_next = []