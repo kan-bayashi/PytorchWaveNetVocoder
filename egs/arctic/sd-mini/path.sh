f264a6db3fb49ebcac8a2c1c261d7c1674280063
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-08-18 22:12:27 +0900

    removed PYTHONPATH

--- egs/arctic/sd-mini/path.sh
+++ egs/arctic/sd-mini/path.sh
@@ -3,4 +3,3 @@ export CUDA_HOME=/usr/local/cuda
 export PRJ_ROOT=../../..
 source $PRJ_ROOT/tools/venv/bin/activate
 export PATH=$PATH:$PRJ_ROOT/src/bin:$PRJ_ROOT/src/utils
-export PYTHONPATH=$PRJ_ROOT/src/nets:$PRJ_ROOT/src/utils
